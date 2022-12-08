2ccc7724129e786316aad08a7dca964ed7ccca5a
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-08 21:16:44 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-         puts("\"programming is like building a multilingual puzzle");
-         return(0);        
+         puts("\"programming is like building a multilingual puzzle,");
+         return (0);        
 }
  
 
