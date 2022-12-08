332efd2181b83f457cf672012b9ccac4b23d37d8
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-08 22:15:17 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art.\n")
 	return (0);
 
 }
