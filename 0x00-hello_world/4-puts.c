9e36928ca200736c6f283cabbc15f7593a5c985b
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-08 21:00:07 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
          puts("\"programming is like building a multilingual puzzle");
