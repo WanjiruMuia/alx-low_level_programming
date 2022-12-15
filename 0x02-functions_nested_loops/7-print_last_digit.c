450b9d5839bf2f048687a756204361c701447e73
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-16 00:38:39 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,33 +1,41 @@
 #include "main.h"
 
-/**
-*times_table -> time table
-*/
-void times_table(void)
-{
-int x, y, k;
 
-for (x = 0; x < 10; x++)
-{
-for (y = 0; y < 10; y++)
-{
-k = x * y;
-if (y == 0)
-_putchar(k + '0');
-if (y != 0 && k < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-} else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+	/**
+	 * times_table - prints the 9 times table, starting with 0
+	 */
+	void times_table(void)
+	{
+		int i, j, k;
+
+
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+
+
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+
+
