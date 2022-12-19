2dfd112a213a0cb57b4b3288fae5bb42fec9e156
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-19 21:11:51 +0300

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,13 @@
 /**
 * print_rev - This funtion prints a reverse string
 * @s: This is the input string
-* return: 0
 */
 void print_rev(char *s)
 {  
 	int index;
 	
 	for (index = 0; s[index] != '\0'; index++)
-	     =
+	       ;
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
