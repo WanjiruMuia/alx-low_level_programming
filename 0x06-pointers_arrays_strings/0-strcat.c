24ec8f5062cb3dc2a161b3343760b483c9bbaa78
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-22 12:51:27 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,29 @@
-cat > main.h
-                                                                                                           
-#ifndef MAIN_H                                                                                                                     
-#define MAIN_H                                                                                                                     
-                                                                                                                                        
-/**                                                                                                                                     
-*File: main.h                                                                                                                    
-*Auth: wanjiru(mercy muia)                                                                                                             
-*Desc: Header file containing prototypes for all functions                                                                            
-*used in the 0x06. C - More pointers, arrays and strings directory.
-*/                                                                                                                                     
-                                                                                                                                        
-int _putchar(char c);                                                                                                                   
-char *_strcat(char *dest, char *src);                                                                                                   
-char *_strncat(char *dest, char *src, int n);                                                                                           
-char *_strncpy(char *dest, char *src, int n);                                                                                           
-int _strcmp(char *s1, char *s2);                                                                                                        
-void reverse_array(int *a, int n);                                                                                                      
-char *string_toupper(char *);                                                                                                           
-char *cap_string(char *);                                                                                                               
-char *leet(char *);                                                                                                                     
-char *rot13(char *);                                                                                                                    
-void print_number(int n);                                                                                                               
-char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                            
-void print_buffer(char *b, int size);                                                                                                   
-                                                                                                                                        
-#endif
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-
+	dest[i] = '\0';
+	return (dest);
+}
