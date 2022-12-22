5d839a86d0db19b887e8d18bc380df1098b08b4d
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-22 13:15:22 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,25 @@
-cat > _putchar.c
+#include "main.h"
+#include <stdio.h>
 
-#include "main.h"                                                                                                                  
-#include <unistd.h>                                                                                                                     
-                                                                                                                                        
-/**                                                                                                                                     
- * _putchar - writes the character c to stdout                                                                                          
- * @c: The character to print                                                                                                           
- *                                                                                                                                      
- * Return: On success 1.                                                                                                                
- * On error, -1 is returned, and errno is set appropriately.                                                                            
- */                                                                                                                                     
-int _putchar(char c)                                                                                                                    
-{                                                                                                                                       
-        return (write(1, &c, 1));                                                                                                       
-}                              
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}                
 
 
 
