5f2147fd4a5ccf93abff2270607ff8f1c7b7c819
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2022-12-22 13:37:26 +0300

    _putchar.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,45 @@
-#include "main.h"
-#include <unistd.h>
+cat > _putchar.c
 
-/**
-*_putchar = writes the character c to stdout
-*@c ; The character to ptint
-*
-*Return: on success
-*on error, =1 is returned, and errno is set appropriately
-*/
-int _putchar(char c)
-{
-return(write(1, $c, 1));
-}
+#include "main.h"                                                                                                                  
+#include <unistd.h>                                                                                                                     
+                                                                                                                                        
+/**                                                                                                                                     
+ * _putchar - writes the character c to stdout                                                                                          
+ * @c: The character to print                                                                                                           
+ *                                                                                                                                      
+ * Return: On success 1.                                                                                                                
+ * On error, -1 is returned, and errno is set appropriately.                                                                            
+ */                                                                                                                                     
+int _putchar(char c)                                                                                                                    
+{                                                                                                                                       
+        return (write(1, &c, 1));                                                                                                       
+}                              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
