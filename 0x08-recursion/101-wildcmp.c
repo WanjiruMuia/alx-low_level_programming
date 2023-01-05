8e880940a6c84105beef544af7ce9054b08c0c8b
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-05 20:53:53 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,33 +6,33 @@ char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);                                                                                                      
                                                                                                                                       
 /**                                                                                                                                   
- * strlen_no_wilds - Returns the length of a string,                                                                                  
- *                   ignoring wildcard characters.                                                                                    
- * @str: The string to be measured.                                                                                                   
- *                                                                                                                                    
- * Return: The length.                                                                                                                
- */
+* strlen_no_wilds - Returns the length of a string,                                                                                  
+*                   ignoring wildcard characters.                                                                                    
+* @str: The string to be measured.                                                                                                   
+*                                                                                                                                    
+* Return: The length.                                                                                                                
+*/
 int strlen_no_wilds(char *str)                                                                                                        
 {                                                                                                                                     
-        int len = 0, index = 0;                                                                                                       
+int len = 0, index = 0;                                                                                                       
                                                                                                                                       
-        if (*(str + index))                                                                                                           
-        {                                                                                                                             
-                if (*str != '*')                                                                                                      
-                        len++;                                                                                                        
+if (*(str + index))                                                                                                           
+{                                                                                                                             
+if (*str != '*')                                                                                                      
+len++;                                                                                                        
                                                                                                                                       
-                index++;                                                                                                              
-                len += strlen_no_wilds(str + index);                                                                                  
-        }                                                                                                                             
+index++;                                                                                                              
+len += strlen_no_wilds(str + index);                                                                                  
+}                                                                                                                             
                                                                                                                                       
-        return (len);                                                                                                                 
+return (len);                                                                                                                 
 }                                                                                                                                     
                                                                                                                                       
 /**                                                                                                                                   
- * iterate_wild - Iterates through a string located at a wildcard                                                                     
- *                until it points to a non-wildcard character.                                                                        
- * @wildstr: The string to be iterated through.                                                                                       
- */                                                                                                                                   
+* iterate_wild - Iterates through a string located at a wildcard                                                                     
+* until it points to a non-wildcard character.                                                                        
+* @wildstr: The string to be iterated through.                                                                                       
+*/                                                                                                                                   
 void iterate_wild(char **wildstr)                                                                                                     
 {                                                                                                                                     
         if (**wildstr == '*')                                                                                                         
