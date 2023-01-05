b2b6d1ec254014f410a2c5d7b23282d573ce12f4
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-05 21:02:05 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -47,17 +47,14 @@ iterate_wild(wildstr);                                                          
 char *postfix_match(char *str, char *postfix)                                                                                         
 {                                                                                                                                     
 int str_len = strlen_no_wilds(str) - 1;                                                                                       
-int postfix_len = strlen_no_wilds(postfix) - 1;                                                                               
-                                                                                                                                      
+int postfix_len = strlen_no_wilds(postfix) - 1;                                                                                                                                                                                                                   
 if (*postfix == '*')                                                                                                          
-iterate_wild(&postfix);                                                                                               
-                                                                                                                                      
+iterate_wild(&postfix);                                                                                                                                                                                                                                    
 if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')                                                           
 {                                                                                                                             
 postfix++;                                                                                                            
 return (postfix_match(str, postfix));                                                                                 
-}                                                                                                                             
-                                                                                                                                      
+}                                                                                                                                                                                                                                                                 
 return (postfix);                                                                                                             
 }
 /**                                                                                                                                   
@@ -74,13 +71,10 @@ if (*s2 == '*')
 {                                                                                                                             
 iterate_wild(&s2);                                                                                                    
 s2 = postfix_match(s1, s2);                                                                                           
-}                                                                                                                             
-                                                                                                                                      
+}                                                                                                                                                                                                                                                                  
 if (*s2 == '\0')                                                                                                              
-return (1);                                                                                                           
-                                                                                                                                      
+return (1);                                                                                                                                                                                                                                                
 if (*s1 != *s2)                                                                                                               
-return (0);                                                                                                           
-                                                                                                                                      
+return (0);                                                                                                                                                                                                                                                
 return (wildcmp(++s1, ++s2));                                                                                                 
 }
