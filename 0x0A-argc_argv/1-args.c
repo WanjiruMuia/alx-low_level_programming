914581207548e89c62c4523a3bbfdb121de388bf
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-11 01:41:01 +0300

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
-#include <stdio.h>                                                                                                                   
+#include <stdio.h>
 #include "main.h"                                                                                                                    
                                                                                                                                      
 /**                                                                                                                                  
- * main - print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
+* main - print the name of the program                                                                                              
+* @argc: Count arguments                                                                                                            
+* @argv: Arguments                                                                                                                  
+*                                                                                                                                   
+* Return: Always 0 (Success)                                                                                                        
+*/                                                                                                                                  
                                                                                                                                      
 int main(int argc, char *argv[])                                                                                                     
 {                                                                                                                                    
