baadebb5c6f1db43b03424e314aeae0781097e32
WanjiruMuia <mercymuia5@gmail.com>
2023-01-10 12:20:50 -0800

    Task 0

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,30 +1,10 @@
-#include "main.h"
-/**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
- */
-unsigned int _strspn(char *s, char *accept)
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char *_strncpy(char *dest, char *src, int n)
 {
-	unsigned int n = 0;
-	int r;
- 
-	while (*s)
-	{
-		for (r = 0; accept[r]; r++)
-		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
-		}
-		s++;
-	}
-	return (n);
+	    return strncpy(dest, src, n);
 }
- 
- 
+
