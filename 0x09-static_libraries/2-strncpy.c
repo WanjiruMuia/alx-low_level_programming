3114a08d66931bfad73d7737dd139877b8ab113a
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 12:06:44 +0300

    2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,11 +1,21 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+ /**
+ * _strncpy - a function that copies two strings
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	    return strncpy(dest, src, n);
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
 }
-
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
+}
