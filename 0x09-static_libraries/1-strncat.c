a264fd522ce915f16925bbf620d990fb2b1536f2
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 12:02:11 +0300

    1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,12 +1,20 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-
+#include "main.h"
+/**
+ *_strncat - concatinates two sstrings using n bytes
+ *
+ *@dest: The destintion string
+ *@src: the source string
+ *@n: The numbe rof bytes contained in source
+ *
+ *Return: A pointer to the resulting string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	    return strncat(dest, src, n);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
-
-
