7c40783a7ebe35549ba444e12882176b094a433f
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 12:13:19 +0300

    3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,11 +1,28 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ * _strspn - length of prefix substring
+ * @s: string to go through
+ * @accept: accepted bytes
+ * Return: returns usigned values
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	    return strspn(s, accept);
+int i;
+int j;
+unsigned int result = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; s[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+result++;
+break;
 }
-
+}
+if (s[j] == '\0')
+return (result);
+}
+return (result);
+}
