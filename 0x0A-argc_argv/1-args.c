bb6c26425907b507c001c04f53f50aba6b9e1009
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 12:25:01 +0300

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include "main.h"
+ #include <stdio.h>
+#define unused(x) (void)(x)
 /**
-*main - print the name of the program
-* @argc: Count arguments
-* @argv: Arguments
-*
-* Return: Always 0 (Success)
-*/
-int main(int argc, char *argv[]) 
+ *main - main function
+ *@argc: arguments count
+ *@argv: arguments vecta
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
 {
-(void) argv; /*Ignore argv*/  
-printf("%i\n", argc - 1);  
-return (0);  
-}
-
-                                                                                                                              
+unused(argv);
+printf("%d\n", argc - 1);
+return (0);
+}                                                                                                              
 
 
