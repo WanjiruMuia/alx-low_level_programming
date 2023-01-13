15262097e56a5b3bfe8092ed9936ce30929cc25e
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 12:04:36 +0300

    2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,11 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ *_strchr - a function that locates a character in a string
+ *@s: string to locate
+ *@c: Character to find
+ *
+ *Return: character value
+ */
 char *_strchr(char *s, char c)
 {
-	    return strchr(s, c);
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + 1);
 }
-
+}
+if (s[i] == c)
+return (s + 1);
+return (0);
+}
