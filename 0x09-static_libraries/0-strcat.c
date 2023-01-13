2c1ec2cbe43a36e457bec7f85024ac124c2f2b1d
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 11:59:07 +0300

    0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,12 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ *_strcat - Concatinates two characters
+ *
+ *@dest: one of the strings
+ *@src: one of the strings
+ *
+ *Return: a pointer to the resulting string dest
+ */
 char *_strcat(char *dest, char *src)
 {
-	    return strcat(dest, src);
+int j = 0, i = 0;
+while (dest[j++])
+i++;
+for (j = 0; src[j]; j++)
+dest[i++] = src[j];
+return (dest);
 }
-
-
