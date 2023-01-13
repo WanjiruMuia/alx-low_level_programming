770a21cec1dfc227a39c599d8dfb980685a18683
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-01-13 12:16:55 +0300

    6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ *_abs - prints the absolute value of an integer
+ *@n: integer value
+*Return: Always 0
+ */
 int _abs(int n)
 {
-	    return abs(n);
+if (n > 0)
+return (n);
+else
+return (n * -1);
 }
-
-
