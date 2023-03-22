8e623db9919595d6a701561b25f7443216b698d0
WanjiruMuia <mercymuia5@gmail.com>
2023-03-22 01:24:07 -0700

    Task 1

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
- *_memset -  a function that fills a memory with a constant bytes
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
  *
- *@n:Size of bytes
- *@s: Memory area returned
- *@b:Constant byte
- *
- *Return: char
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
