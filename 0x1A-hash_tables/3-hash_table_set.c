0f76288db662edc88f0a4cca289ee81b81fd84db
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-07 12:45:03 +0300

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,90 +2,93 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+/**
+ * set_pair - mallocs a key/value pair to the hash table.
+ * @key: the key, a string that cannot be empty.
+ * @value: the value associated with the key, can be an empty string.
+ *
+ * Return: pointer to the new node.
+ */
+hash_node_t *set_pair(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->key = malloc(strlen(key) + 1);
+	if (node->key == NULL)
+		return (NULL);
+	node->value = malloc(strlen(value) + 1);
+	if (node->value == NULL)
+		return (NULL);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	return (node);
+}
 
 /**
-typedef struct hash_node_s {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
+ * set_pair_only - (no collision) set key:value pair to first array element
+ * @ht: pointer to the hash table.
+ * @key: the key, a string that cannot be empty.
+ * @value: the value associated with the key, can be an empty string.
+ * @index: the key's index.
+ *
+ * Return: the node, or NULL if failed.
+ */
+int set_pair_only(hash_table_t *ht, const char *key,
+		  const char *value, unsigned long int index)
+{
+	hash_node_t *node = set_pair(key, value);
 
-typedef struct hash_table_s {
-    unsigned long int size;
-    hash_node_t **array;
-} hash_table_t;
-
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-hash_node_t *hash_node_create(const char *key, const char *value);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
-unsigned long int hash_djb2(const unsigned char *str) {
-    unsigned long int hash = 5381;
-    int c;
-    while ((c = *str++) != 0) {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return hash;
+	if (node == NULL)
+		return (0);
+	node->next = NULL;
+	ht->array[index] = node;
+	return (1);
 }
 
-hash_node_t *hash_node_create(const char *key, const char *value) {
-    hash_node_t *node = malloc(sizeof(hash_node_t));
-    if (node == NULL) {
-        return NULL;
-    }
-    node->key = strdup(key);
-    node->value = strdup(value);
-    node->next = NULL;
-    return node;
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: a pointer to the hash table array.
+ * @key: the key, a string that cannot be empty.
+ * @value: the value associated with the key, can be an empty string.
+ *
+ * Return: 1 on success, 0 on error.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (key == NULL || ht == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (set_pair_only(ht, key, value, index));
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			if (strcmp(node->value, value) == 0)
+				return (1);
+			free(node->value);
+			node->value = malloc(strlen(value) + 1);
+			if (node->value == NULL)
+				return (0);
+			strcpy(node->value, value);
+			return (1);
+		}
+		node = node->next;
+	}
+	if (node == NULL)
+	{
+		node = set_pair(key, value);
+		if (node == NULL)
+			return (0);
+		node->next = ht->array[index];
+		ht->array[index] = node;
+		return (1);
+	}
+	return (0);
 }
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
-    if (ht == NULL || key == NULL || strlen(key) == 0) {
-        return 0;
-    }
-
-    unsigned long int index = hash_djb2((const unsigned char *) key) % ht->size;
-
-    hash_node_t *node = ht->array[index];
-    while (node != NULL) {
-        if (strcmp(node->key, key) == 0) {
-            free(node->value);
-            node->value = strdup(value);
-            return 1;
-        }
-        node = node->next;
-    }
-
-    hash_node_t *new_node = hash_node_create(key, value);
-    if (new_node == NULL) {
-        return 0;
-    }
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-    return 1;
-}
-
-int main() {
-    hash_table_t *hash_table = hash_table_create(10);
-    if (hash_table == NULL) {
-        printf("Failed to create hash table\n");
-        return 1;
-    }
-
-    if (!hash_table_set(hash_table, "key1", "value1")) {
-        printf("Failed to set key1 to value1\n");
-    }
-
-    if (!hash_table_set(hash_table, "key2", "value2")) {
-        printf("Failed to set key2 to value2\n");
-    }
-
-    if (!hash_table_set(hash_table, "key1", "value3")) {
-        printf("Failed to update key1 to value3\n");
-    }
-
-    return 0;
-}
-
