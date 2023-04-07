5a16d5515daddc2e0102d2848e209756af2331ff
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-07 12:39:00 +0300

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,24 +1,12 @@
 #include "hash_tables.h"
-
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
+* key_index - gives the index of a key.
+* @key: the key, a string.
+* @size: the size of the array of the hash table.
+*
+* Return: Index number for the key/value pair in the hash table array.
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    char *s;
-    unsigned long int hash_table_array_size;
-
-    hash_table_array_size = 1024;
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));  
-    return (EXIT_SUCCESS);
+	return (hash_djb2(key) % size);
 }
