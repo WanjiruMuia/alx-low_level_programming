77638a9002d963faf029a26d1de8108119398495
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-11 17:35:39 +0300

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -8,29 +8,16 @@
  *
  * Return: the index found or -1
  */
-int jump_search(int *array, size_t size, int value)
-{
-	size_t i = 0, j = sqrt(size);
-
-	if (!array)
-		return (-1);
-
-	while (i < size && array[i] < value)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		i += j;
-	}
-
-	j = i - j;
-	printf("Value found between indexes [%lu] and [%lu]\n", j, i);
-	i = i >= size ? size - 1 : i;
-	while (j <= i)
-	{
-		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
-		if (array[j] == value)
-			return (j);
-		j++;
-	}
-	return (-1);
-}
+def jump_search(arr,x):
+ n=len(arr)
+	step+= int(math.sqrt(n))
+	if prev>=n:
+	return -1
+while arr[prev] < x:
+	prev += 1
+	if prev == min(step,n):
+	return -1
+if arr[prev] ==x:
+ return prev
+return -1 
 		
